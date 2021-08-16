24490a9e6ab3e80a819bf81bee07760938d0f83a
Mahmod Hammad <hodaman2012@gmail.com>
2021-08-16 18:08:17 +0200

    Catch the bug :)

--- src/three/setup.js
+++ src/three/setup.js
@@ -1,68 +1,65 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "stats-js";
-import TWEEN from "@tweenjs/tween.js";
 import { displayCoards } from "./helper.js";
-import settings from "./variables/settings";
+import settings from "./variables/settings.js";
+import Stats from "stats-js";
 import {addToScene} from "./sceneItems"
+
 THREE.Cache.enabled = true;
 
 const stats = new Stats();
 
-let projectName = "loca";
+// For 100% width&Height
 let width = window.innerWidth;
 let height = window.innerHeight;
 // ----------------------------------------------> render
 const renderer = new THREE.WebGLRenderer({
   alpha: true,
   powerPreference: "high-performance",
-  antialias: false,
+  antialias: settings.enableAntialias,
 });
 renderer.setPixelRatio(settings.quality);
+
 function render() {
   renderer.render(scene, camera);
-  TWEEN.update();
 }
+
 // ----------------------------------------------> scene
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xaaaaaa);
+scene.background = new THREE.Color(0xffffff);
+
 // ----------------------------------------------> camera
 const camera = new THREE.PerspectiveCamera(
   40, // fov = field of view
   width / height, // aspect ratio
   0.001, // near plane
-  8000 // far plane
+  80000 // far plane
 );
-camera.position.set(0, 0, 8);
+camera.position.set(0, 10, 80);
 
 // ----------------------------------------------> controls
 const controls = new OrbitControls(camera, renderer.domElement);
-function setupControls(speed) {
-  let ctrSpeed = speed || settings.ctrlSpeed;
-  controls.zoomSpeed = ctrSpeed;
-  controls.panSpeed = ctrSpeed;
-  controls.rotateSpeed = ctrSpeed;
+function setupControls() {
+  controls.target = new THREE.Vector3(0, 0, 0);
+  const{ctrlSpeed ,maxZoom,minZoom,maxPolarAngle,minPolarAngle,autoRotate,autoRotateSpeed,enableDamping,dampingFactor}=settings
 
-  controls.target = new THREE.Vector3(0, 1, 0);
+  controls.zoomSpeed = ctrlSpeed;
+  controls.panSpeed = ctrlSpeed;
+  controls.rotateSpeed = ctrlSpeed;
 
-  controls.maxDistance = settings.maxZoom;
-  controls.minDistance = settings.minZoom;
+  controls.maxDistance = maxZoom;
+  controls.minDistance = minZoom;
 
-  controls.maxPolarAngle = settings.maxPolarAngle;
-  controls.minPolarAngle = settings.minPolarAngle;
+  controls.maxPolarAngle = maxPolarAngle;
+  controls.minPolarAngle = minPolarAngle;
 
-  controls.autoRotate = settings.autoRotate;
-  controls.autoRotateSpeed = settings.autoRotateSpeed;
+  controls.autoRotate = autoRotate;
+  controls.autoRotateSpeed = autoRotateSpeed;
 
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.05;
+  controls.enableDamping = enableDamping;
+  controls.dampingFactor =dampingFactor;
 }
 
-function stopTweenAnimation() {
-  TWEEN.removeAll();
-}
-
-controls.addEventListener("start", stopTweenAnimation);
 // ----------------------------------------------> resize
 const handleWindowResize = () => {
   width = window.innerWidth;
@@ -72,26 +69,22 @@ const handleWindowResize = () => {
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
 };
+
 // ----------------------------------------------> setup
-const sceneSetup = (root, theprojectName) => {
-  projectName = theprojectName;
+const sceneSetup = (root) => {
   renderer.setSize(width, height);
   root.appendChild(renderer.domElement);
   window.addEventListener("resize", handleWindowResize);
+
   if (settings.developmentModel) {
-    displayCoards();
-  }
-  setupControls();
-  if (settings.developmentModel) {
+    displayCoards(100,10);
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild(stats.dom);
   }
+
+  setupControls();
   addToScene()
 };
-
-function setProjectName(newprojectName) {
-  projectName = newprojectName;
-}
 
 export {
   sceneSetup,
@@ -100,6 +93,4 @@ export {
   render,
   camera,
   stats,
-  projectName,
-  setProjectName,
 };
